dbfe9edcbb8282caef3fa389f0402e3f2c1c2248
mimigithub-prog <miriamnigeria44@gmail.com>
2023-07-28 11:15:04 +0100

    Project IOS and Android setup

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:lostweb/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const LostWebApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
